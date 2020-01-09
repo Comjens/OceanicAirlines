6ac70e567763d304eebde943f544e9fe4e72cac6
SirTipzy <thomas.bjelbo.thomsen@gmail.com>
2020-01-09 13:25:54 +0100

    Context

---
+++ Data/ParcelContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OceanicAirlines.Model;
+
+namespace OceanicAirlines.Data
+{
+    public class ParcelContext : DbContext
+    {
+        public ParcelContext(DbContextOptions<ParcelContext> options) : base(options)
+        {
+        }
+
+        public DbSet<LocationModel> Locations { get; set; }
+        public DbSet<RouteModel> Routes { get; set; }
+        public DbSet<ShipmentModel> Shipments { get; set; }
+        public DbSet<ShipmentTypeModel> ShipmentTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShipmentModel>().ToTable("Shipment");
+            modelBuilder.Entity<ShipmentTypeModel>().ToTable("ShipmentType");
+            modelBuilder.Entity<RouteModel>().ToTable("Route");
+            modelBuilder.Entity<LocationModel>().ToTable("Location");
+        }
+    }
+}
+
--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OceanicAirlines.Data;
 
 namespace OceanicAirlines
 {
@@ -24,6 +26,8 @@ namespace OceanicAirlines
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ParcelContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ParcelContext")));
         }
 
         // This method gets called by the runtime. Use this method To configure the HTTP request pipeline.
